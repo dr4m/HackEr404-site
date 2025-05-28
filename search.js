e0f4fc6e24e36b79c52ad2bf6aeae3065c244a86
Hacker404 <46074242+dr4m@users.noreply.github.com>
2025-05-29 01:07:45 +0530

    Update search.js

--- search.js
+++ search.js
@@ -4,6 +4,7 @@
     { title: "SQLMap Usage", url: "https://hacker404.site/tools/sqlmap.html" },
     { title: "Facebook Hacking", url: "https://hacker404.site/tools/fbhack1.html" },
     { title: "Burp Suite Basics", url: "https://hacker404.site/tools/burpsuitetut01.html" },
+    { title: "Termux Tricks", url: "https://hacker404.site/termux/termuxtrick1.html" },
     // Add more as needed
   ];
 
